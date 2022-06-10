a6d3a3acb193e5e0213d9d3bc04b3f072b60cdda
ABOUHALI <abouhaliayman3@gmail.com>
2022-06-10 20:39:31 +0100

    some changes

---
+++ src/main/java/web/Login.java
@@ -0,0 +1,81 @@
+package web;
+
+
+import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.mysql.cj.Session;
+
+import dao.LoginDAO;
+import model.Users;
+
+/**
+ * Servlet implementation class LoginSER
+ */
+@WebServlet(name="cs",urlPatterns={"/Login"})
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	LoginDAO ld = new LoginDAO();
+	
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("GET");
+		doPost(request, response);
+
+	}
+
+	/*
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 * response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("POST");
+		HttpSession session = request.getSession();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		Users user = new Users();
+		user.setLogin(username);
+		user.setMdp(password);
+		if (ld.validation(user)) {
+			String role = ld.Role(user);
+			if(role.equals("admin")) {
+				System.out.println(4);
+				session.setAttribute("user-admin", user);
+				System.out.println(session.getAttribute("user-admin"));
+				this.getServletContext().getRequestDispatcher("").forward(request, response);
+
+			}else if (role.equals("client")){
+				session.setAttribute("user-admin", user);
+				System.out.println(session.getAttribute("user-client"));
+				this.getServletContext().getRequestDispatcher("").forward(request, response);
+			}
+			}
+		else {
+			this.getServletContext().getRequestDispatcher("/errorLOGIN.jsp").forward(request, response);
+		}
+	}
+	}
+
